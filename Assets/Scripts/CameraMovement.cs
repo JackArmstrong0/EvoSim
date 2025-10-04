5151d91b52d75ef142045e5146cfa027a7d8a53d
Jack Armstrong <jarmers.armstrong@gmail.com>
2025-10-04 21:46:50 +0100

    Set up Camera and Basic Scene
    Created a ground and applied ground material. Added CameraMovement script to Main Camera with HandleMovement() and HandleRotation() methods.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float shiftMult = 2f;
+    public float mouseSens = 100f;
+    private float camAngle = 0f;
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleMovement()
+    {
+        float speed = moveSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            speed *= shiftMult;
+        }
+
+        // Controlled with WASD or Arrow Keys
+        float stepX = Input.GetAxisRaw("Horizontal");
+        float stepZ = Input.GetAxisRaw("Vertical");
+        float stepY = 0f;
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            stepY = 1f;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            stepY = -1f;
+        }
+
+        Vector3 stepDirection = transform.right * stepX + transform.up * stepY + transform.forward * stepZ;
+        transform.position += stepDirection * speed * Time.deltaTime;
+    }
+    void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX); // Rotates Yaw, Horizontally
+
+        // Rotates Pitch, Vertically
+        camAngle -= mouseY;
+        camAngle = Mathf.Clamp(camAngle, -80, 80); // Prevents flipping upside down
+
+        transform.localEulerAngles = new Vector3(camAngle, transform.localEulerAngles.y, 0f);
+    }
+}
